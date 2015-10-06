a348d75f9a22f352596daadea3cbe7dcdf00de61
Satoshi Nagayasu <snaga@uptime.jp>
2015-10-06 17:40:48 +0900

    Add setup scripts and fluentd config for audit log database ane table.

---
+++ conf/auditlogdb.sql
@@ -0,0 +1,31 @@
+DROP DATABASE auditlogdb;
+CREATE DATABASE auditlogdb;
+ALTER DATABASE auditlogdb SET pgaudit.log = 'privilege,user,definition,config,admin,function';
+
+-- Writer
+CREATE ROLE audit001 WITH NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN NOREPLICATION;
+\password audit001
+
+-- Viewer
+CREATE ROLE audit102 WITH NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN NOREPLICATION;
+\password audit102
+
+\c auditlogdb postgres
+
+DROP TABLE auditlog_raw;
+CREATE TABLE auditlog_raw (
+    tag text
+    ,time Timestamptz
+    ,record jsonb
+);
+
+REVOKE ALL ON auditlog_raw FROM audit001;
+GRANT INSERT ON auditlog_raw TO audit001;
+
+REVOKE ALL ON auditlog_raw FROM audit102;
+GRANT SELECT ON auditlog_raw TO audit102;
+
+\c auditlog audit001
+
+SELECT * FROM auditlog_raw;
+
