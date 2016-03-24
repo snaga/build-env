143d20f4d2e29f12277e740a737cb9e165d1ee3b
Satoshi Nagayasu <snaga@uptime.jp>
2016-03-24 01:54:36 -0500

    Fix to specify rake version to work with ruby 1.8.7.

--- serverspec_setup.sh
+++ serverspec_setup.sh
@@ -3,5 +3,5 @@
 yum install -y rubygems
 gem install net-ssh -v 2.9.2 --no-ri --no-rdoc -V
 gem install serverspec --no-ri --no-rdoc -V
-gem install rake --no-ri --no-rdoc -V
+gem install rake -v 10.5.0 --no-ri --no-rdoc -V
 
