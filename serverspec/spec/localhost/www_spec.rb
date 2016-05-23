6bb40c8a40fb4a75ad9d411b8bf9277ec289b0d0
Satoshi Nagayasu <snaga@uptime.jp>
2016-05-24 08:49:05 +0900

    Add a new spec file for web server.

---
+++ serverspec/spec/localhost/www_spec.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'spec_helper'
+
+# RPM packages
+# i:yum install -y httpd
+describe package('httpd') do
+  it { should be_installed }
+end
+
+# robots.txt
+# i:vi /var/www/html/robots.txt
+# User-agent: *
+# Disallow: /
+describe file('/var/www/html/robots.txt') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+# HTTPD service
+# i:service httpd start
+# i:chkconfig httpd on
+describe service('httpd') do
+  it { should be_enabled }
+  it { should be_running }
+end
