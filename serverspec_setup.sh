12b2a36a89066b2e78b56e84373d46aa8022575d
Satoshi Nagayasu <snaga@uptime.jp>
2016-06-23 11:38:34 -0500

    Fix to install json gem explicitly.

--- serverspec_setup.sh
+++ serverspec_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 yum install -y rubygems
+gem install json --no-ri --no-rdoc -V
 gem install net-ssh -v 2.9.2 --no-ri --no-rdoc -V
 gem install serverspec --no-ri --no-rdoc -V
 gem install rake -v 10.5.0 --no-ri --no-rdoc -V
