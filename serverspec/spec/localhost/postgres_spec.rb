c241e76e3cb8b04c555ccf2df18146d6b0fb5954
Satoshi Nagayasu <snaga@uptime.jp>
2016-07-03 20:33:16 +0900

    Fix postgres_spec.

--- serverspec/spec/localhost/postgres_spec.rb
+++ serverspec/spec/localhost/postgres_spec.rb
@@ -27,43 +27,6 @@ describe package('pgdg-redhat95') do
   it { should be_installed }
 end
 
-# PostgreSQL 9.1 (oldest one)
-# i:yum install -y postgresql91 postgresql91-contrib postgresql91-devel postgresql91-libs postgresql91-plperl postgresql91-plpython postgresql91-server
-describe package('postgresql91') do
-  it { should be_installed }
-end
-
-describe package('postgresql91-contrib') do
-  it { should be_installed }
-end
-
-describe package('postgresql91-devel') do
-  it { should be_installed }
-end
-
-describe package('postgresql91-libs') do
-  it { should be_installed }
-end
-
-describe package('postgresql91-plperl') do
-  it { should be_installed }
-end
-
-describe package('postgresql91-plpython') do
-  it { should be_installed }
-end
-
-describe package('postgresql91-server') do
-  it { should be_installed }
-end
-
-# i:service postgresql-9.1 start
-# i:chkconfig postgresql-9.1 on
-describe service('postgresql-9.1') do
-  it { should_not be_enabled }
-  it { should_not be_running }
-end
-
 # PostgreSQL 9.5
 # i:yum install -y postgresql95 postgresql95-contrib postgresql95-devel postgresql95-libs postgresql95-plperl postgresql95-plpython postgresql95-server
 describe package('postgresql95') do
